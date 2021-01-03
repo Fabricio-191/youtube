d5fb11f909271ded371d96a0ee756befa29ef68c
Fabricio-191 <faelmejor186@gmail.com>
2021-01-03 18:33:09 -0300

    -

--- test/test.js
+++ test/test.js
@@ -9,7 +9,6 @@ const { getVideo, getPlaylist, search } = require('../')
 	});
 
 (async () => {
-	throw new Error('a');
 	let data = {
 		video: await getVideo(
 			'https://www.youtube.com/watch?v=H2wCwdHk-ao'
@@ -21,7 +20,7 @@ const { getVideo, getPlaylist, search } = require('../')
 		search: await search('Node.js', { quantity: 40 })
 	};
 
-	return; //console.log(data);
+	//return; //console.log(data);
 
 	if(fs.existsSync('./test')) return; //running from the test script
 
